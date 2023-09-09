f7e7846250f628812e0eb38821085a2300b26785
Fabricio Silva <fabricioj49@gmail.com>
2023-09-09 18:03:28 -0300

    Initial commit

---
+++ src/main/java/com/example/DesafioLapes/DesafioLapesApplication.java
@@ -0,0 +1,13 @@
+package com.example.DesafioLapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLapesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLapesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DesafioLapes/DesafioLapesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DesafioLapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLapesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
