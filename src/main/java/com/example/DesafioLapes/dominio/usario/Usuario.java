e5e706a51d46def66b1d7817b320d51146246bf8
Fabricio Silva <fabricioj49@gmail.com>
2023-09-13 23:21:41 -0300

    feat(usuario): adicionado camada de autorizacao

--- src/main/java/com/example/DesafioLapes/dominio/usario/Usuario.java
+++ src/main/java/com/example/DesafioLapes/dominio/usario/Usuario.java
@@ -2,6 +2,12 @@ package com.example.DesafioLapes.dominio.usario;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity(name="usuario")
@@ -11,7 +17,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -35,13 +41,56 @@ public class Usuario {
 
     }
 
+    public Usuario(String nome, String email, String senha, UsuarioCargo cargo) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cargo = cargo;
+    }
+
     @PrePersist
     public void persistencia() {
         if (this.cargo == null) {
-            this.cargo = UsuarioCargo.COMUM;
+            this.cargo = UsuarioCargo.valueOf("ADMIN");// temporario
         }
         if (this.saldo == null) {
             this.saldo = 0.0f;
         }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.cargo == UsuarioCargo.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return nome;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
--- src/main/java/com/example/DesafioLapes/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/example/DesafioLapes/repositorios/UsuarioRepositorio.java
@@ -2,10 +2,12 @@ package com.example.DesafioLapes.repositorios;
 
 import com.example.DesafioLapes.dominio.usario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UsuarioRepositorio extends JpaRepository<Usuario,String> {
 
-    Optional<Usuario> findById(String id);
+    UserDetails findByEmail(String email);
+    Optional<Boolean> existsUsuarioByEmail(String email);
 }
