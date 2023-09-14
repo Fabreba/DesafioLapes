1433c78ca93ac66c80b1b94199f643a4fd380757
Fabricio Silva <fabricioj49@gmail.com>
2023-09-14 10:49:09 -0300

    feat(produto): adicionada o repositorio e DTO de produto

---
+++ src/main/java/com/example/DesafioLapes/dominio/produto/ProdutoDTO.java
@@ -0,0 +1,4 @@
+package com.example.DesafioLapes.dominio.produto;
+
+public record ProdutoDTO(String nome, Float preco, String nome_categoria) {
+}
---
+++ src/main/java/com/example/DesafioLapes/repositorios/ProdutoRepositorio.java
@@ -0,0 +1,11 @@
+package com.example.DesafioLapes.repositorios;
+
+import com.example.DesafioLapes.dominio.produto.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProdutoRepositorio extends JpaRepository<Produto,Long> {
+
+    Optional<Produto> findByNome(Long nome);
+}
