abb4f0ba96b80783c77f19c6d6bce65d201644f6
Fabricio Silva <fabricioj49@gmail.com>
2023-09-13 23:20:24 -0300

    feat: removido o metodo registrarUsuario

--- src/main/java/com/example/DesafioLapes/controladores/ControladorUsuario.java
+++ src/main/java/com/example/DesafioLapes/controladores/ControladorUsuario.java
@@ -1,12 +1,10 @@
 package com.example.DesafioLapes.controladores;
 
-import com.example.DesafioLapes.dominio.usario.Usuario;
-import com.example.DesafioLapes.dominio.usario.UsuarioDTO;
+
 import com.example.DesafioLapes.repositorios.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/usuario")
@@ -18,11 +16,6 @@ public class ControladorUsuario {
         var todosUsarios = repositorio.findAll();
         return ResponseEntity.ok(todosUsarios);
     }
-    @PostMapping
-    public ResponseEntity registrarUsuario(@RequestBody @Valid UsuarioDTO data){
-        Usuario novoUsuario = new Usuario(data);
-        repositorio.save(novoUsuario);
-        return ResponseEntity.ok("Deubom");
-    }
+
 
 }
