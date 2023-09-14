92cb4c0382f832bfb366e734cae5e44788e890b2
Fabricio Silva <fabricioj49@gmail.com>
2023-09-14 08:16:03 -0300

    feat: adicionado as migrations

---
+++ src/main/resources/db/migration/V1__create-table-usuario.sql
@@ -0,0 +1,7 @@
+create table usuario(
+      id TEXT PRIMARY KEY ,
+      nome TEXT NOT NULL,
+      email TEXT NOT NULL,
+      senha TEXT NOT NULL,
+      cargo TEXT NOT NULL
+);
---
+++ src/main/resources/db/migration/V2__create-table-produto.sql
@@ -0,0 +1,6 @@
+create table produto(
+    id INTEGER PRIMARY KEY ,
+    nome TEXT NOT NULL,
+    preco FLOAT NOT NULL,
+    nome_categoria TEXT NOT NULL
+);
