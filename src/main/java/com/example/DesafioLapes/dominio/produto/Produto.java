e60ece5360acc39dff05b687bf5b48aa33094210
Fabricio Silva <fabricioj49@gmail.com>
2023-09-14 11:38:40 -0300

    fix(produto): corrigida o tipo do ID e implementado o construtor

--- src/main/java/com/example/DesafioLapes/dominio/produto/Produto.java
+++ src/main/java/com/example/DesafioLapes/dominio/produto/Produto.java
@@ -12,9 +12,16 @@ import lombok.*;
 @EqualsAndHashCode(of = "id")
 public class Produto {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
     @Column(unique = true)
     private String nome;
+    private Float preco;
     private String nome_categoria;
+
+    public Produto(String nome, Float preco, String nome_categoria) {
+        this.nome = nome;
+        this.preco = preco;
+        this.nome_categoria = nome_categoria;
+    }
 }
